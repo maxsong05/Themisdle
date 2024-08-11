4bdb969642cf14eeb291eae964560a91422780d9
maxsong05 <mxsongx@gmail.com>
2024-08-11 17:30:08 -0400

    j

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,18 @@
+/*function handleClick() {
+  console.log("hi");
+}
+
 function Button() {
   return (
     <>
       <a
-        href="https://tot.wiki/wiki/Cards"
+        href="#"
         className="btn btn-outline-success m-2"
         type="button"
-        target="_blank" //opens in new tab
+        //target="_blank" //opens in new tab
+        onClick={handleClick}
       >
-        Card Mode
+        Play Card Mode
       </a>
 
       <a
@@ -16,9 +21,41 @@ function Button() {
         type="button"
         target="_blank" //opens in new tab
       >
-        Skills Mode
+        Wiki
       </a>
     </>
+  );
+}*/
+import { useState } from "react";
+
+function Button() {
+  const [showImage, setShowImage] = useState(false);
+
+  const handleClick = () => {
+    setShowImage(true); // Update the state to show the image
+  };
+
+  return (
+    <div>
+      <a
+        href="#"
+        className="btn btn-outline-success m-2"
+        type="button"
+        onClick={handleClick}
+      >
+        Play Card Mode
+      </a>
+
+      {/* Conditionally render the image */}
+      <br />
+      {showImage && (
+        <img
+          src="https://tot.wiki/images/f/f2/Artem_%22Staying_Humble%22_preview.png" // Replace with your image path
+          alt="Card Name" // Replace with a description of the image
+          style={{ marginTop: "5px", width: "172px", height: "351px" }} // Adjust the style as needed
+        />
+      )}
+    </div>
   );
 }
 
---
+++ src/getCardList.py
@@ -0,0 +1,21 @@
+##This is simply a python file that returns the list of cards
+import csv
+from random import randint
+
+cardcsv = "public/allCards.csv"
+cardreader = open(cardcsv, 'r')
+listcards = open('cardlist.csv','w')
+cardlist = []
+
+next(cardreader) #skips the header
+for line in cardreader:
+    details = line.split(',')
+    mlead = details[2].split('"')[1] #should look like 'Luke '
+    cardname = details[2].split('"')[3]
+    name = mlead + cardname
+    url, type = details[1],details[3].strip()
+
+    cardlist.append([name, url, type])
+
+
+print(cardlist)
